0b584945099df7840d83d2646b03a45cddee30d1
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-11-22 17:00:21 +0800

    Update Navigation Bar

---
+++ TODO.md
@@ -0,0 +1,14 @@
+# Project TODOs
+
+## Tasks
+
+- [ ] Add Navigation Bar when you have are logged in
+- [ ] Add Navigation Bar componenets when you are not logged in
+
+## Bugs
+
+- [ ] Fix Navigation Bar Issues
+
+## Enhancements
+
+- [ ] Improve Navigation Bar
--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import CreateAssessment from './pages/CreateAssessment';
 import Header from './components/Header';
 
+
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.withCredentials = true;
+
+const client = axios.create({
+  baseUrl: "http://localhost:8000/api",
+})
+
 function App() {
 
-  return (
-    <Router>
-      <div>
-        <Header />
-        <NavigationButton />
-        <Routes>
-          <Route path="/create-assessment" element={<CreateAssessment />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+  const [currentUser, setCurrentUser] = useState();
+  const [registrationToggle, setRegistrationToggle] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  if (currentUser) {
+    return (
+      <Router>
+        <div>
+          <Header currentUser/>
+          <NavigationButton />
+          <Routes>
+            <Route path="/create-assessment" element={<CreateAssessment />} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  } else {
+    return (
+      <Router>
+        <div>
+          <Header />
+          <NavigationButton />
+          <Routes>
+            <Route path="/create-assessment" element={<CreateAssessment />} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
 
 function NavigationButton() {
